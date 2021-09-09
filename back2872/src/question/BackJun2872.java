72df4ccf3fda00efe50683ebb53462538421df2b
ryukiyoun <ryukiyoun@naver.com>
2021-09-09 12:11:26 +0900

    feat : 백준 2872번 알고리즘 풀이
    BackJun2872 Class
     - 최초 입력된 책들의 순서를 역순으로 변경하여 뒤에서 부터 정렬이 안된 수 만큼 이동이 필요하므로 순회를 통해 이동 횟수를 구함

---
+++ back2872/src/question/BackJun2872.java
@@ -0,0 +1,39 @@
+package question;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BackJun2872 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int bookCnt = Integer.parseInt(br.readLine());
+
+        List<Integer> bookList = new ArrayList<>();
+
+        for(int i=0; i<bookCnt; i++)
+            bookList.add(Integer.parseInt(br.readLine()));
+
+        int result = 0;
+
+        Collections.reverse(bookList);
+
+        int target = bookCnt;
+
+        for(int i=0; i<bookCnt; i++){
+            if(bookList.get(i) == target)
+                target--;
+            else
+                result++;
+        }
+
+        bw.write(result + "\n");
+        bw.flush();
+    }
+}
