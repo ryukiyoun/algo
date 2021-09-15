bd05e755377606c3230b60df14c297ce1069c317
ryukiyoun <ryukiyoun@naver.com>
2021-09-15 22:59:31 +0900

    feat : 백준 9655번 알고리즘 풀이
    Main Class
     - DP를 이용한 풀이 추가

--- back9655/src/question/Main.java
+++ back9655/src/question/Main.java
@@ -10,11 +10,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        if((Integer.parseInt(br.readLine()) & 1) == 1)
+        /*간단 문제 풀이*/
+        /*if((Integer.parseInt(br.readLine()) & 1) == 1)
             bw.write("SK\n");
         else
             bw.write("CY\n");
 
+        bw.flush();*/
+
+        /*DP를 이용하여 문제 풀이*/
+        int rockCnt = Integer.parseInt(br.readLine());
+
+        boolean[] dp = new boolean[rockCnt+1];
+
+        if(rockCnt>1)
+            dp[2] = true;
+
+        for(int i=4; i<=rockCnt; i++){
+            dp[i] = !dp[i-1];
+        }
+
+        if(dp[rockCnt])
+            bw.write("CY\n");
+        else
+            bw.write("SK\n");
         bw.flush();
     }
 }
