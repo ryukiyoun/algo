a4f78d9c148c755823dc4e38411d81688998d6f8
ryukiyoun <ryukiyoun@naver.com>
2021-09-29 00:03:01 +0900

    feat : 백준 2579번 알고리즘 풀이
    Main Class
     - 속도 소폭 감소형으로 변경

--- back2579/src/question/Main.java
+++ back2579/src/question/Main.java
@@ -12,18 +12,21 @@ public class Main {
 
         int num = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[num + 1];
-        int[] arr = new int[num + 1];
+        int[] dp = new int[num+1];
 
-        for (int i = 1; i <= num; i++)
-            arr[i] = Integer.parseInt(br.readLine());
+        int postNum = 0;
+        int nowNum;
+        dp[1] = Integer.parseInt(br.readLine());
+        if (num > 1) {
+            postNum = Integer.parseInt(br.readLine());
+            dp[2] = dp[1] + postNum;
+        }
 
-        dp[1] = arr[1];
-        if (num > 1)
-            dp[2] = arr[1] + arr[2];
-
-        for (int i = 3; i <= num; i++)
-            dp[i] = Math.max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i]);
+        for (int i = 3; i <=num; i++) {
+            nowNum = Integer.parseInt(br.readLine());
+            dp[i] = Math.max(dp[i - 2] + nowNum, dp[i - 3] + postNum + nowNum);
+            postNum = nowNum;
+        }
 
         bw.write(dp[num] + "\n");
         bw.flush();
