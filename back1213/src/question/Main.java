b228e5531dbb4f5ae7dcc811346ffdae0912fe8e
ryukiyoun <ryukiyoun@naver.com>
2021-10-07 23:56:41 +0900

    feat : 백준 1213번 알고리즘 풀이
    Main Class
     - 문자열에서 각 문자들의 짝수와 홀수 여부를 판단 홀수는 1개의 문자 까지만 가능 하므로 홀수인 문자가 2개 이상이면 Sorry출력 이후는 사전 순이 되어야 하므로 알파뱃 역순으로 문자가 있는 경우 반만큼만 append 후 만들어진 문자열의 reverse 문자열과 합친다 만약 홀수의 문자가 있으면 가운데 추가하여 풀이

---
+++ back1213/src/question/Main.java
@@ -0,0 +1,62 @@
+package question;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Palindrome palindrome = new Palindrome(br.readLine());
+
+        System.out.println(palindrome.makePalindrome());
+    }
+
+    public static class Palindrome {
+        int[] alpha = new int[26];
+        String[] nameArr;
+
+        public Palindrome(String name) {
+            nameArr = new String[name.length()];
+
+            for (char sub : name.toCharArray())
+                alpha[sub - 65]++;
+        }
+
+        public boolean isPalindrome() {
+            boolean odd = false;
+            for (int i = 0; i < 26; i++) {
+                if ((alpha[i] & 1) == 1) {
+                    if (!odd)
+                        odd = true;
+                    else
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public String makePalindrome() {
+            if (!isPalindrome())
+                return "I'm Sorry Hansoo";
+
+            StringBuilder halfPalindromeNameBuilder = new StringBuilder();
+
+            int oddIndex = -1;
+            for (int i = 25; i > -1; i--) {
+                if((alpha[i] & 1) == 1)
+                    oddIndex = i;
+
+                halfPalindromeNameBuilder.append(Character.toString(i + 65).repeat(alpha[i]/2));
+            }
+
+            String halfPalindromeName = halfPalindromeNameBuilder.toString();
+            halfPalindromeNameBuilder.reverse().append(halfPalindromeName);
+
+            if(oddIndex != -1)
+                halfPalindromeNameBuilder.insert(nameArr.length / 2, Character.toString(oddIndex + 65));
+
+            return halfPalindromeNameBuilder.toString();
+        }
+    }
+}
