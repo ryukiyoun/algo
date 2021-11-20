bc4c96f2e434cd766fc11a836563735db133a207
ryukiyoun <ryukiyoun@naver.com>
2021-11-20 14:33:58 +0900

    feat : 프로그래머스 정수삼각형 알고리즘 풀이
    Solution Class
     - i==0인경우와 i==j인 경우를 if문이 아닌 loop안으로 변경

--- programmers정수삼각형/src/question/Main.java
+++ programmers정수삼각형/src/question/Main.java
@@ -9,18 +9,14 @@ public class Main {
     public static class Solution {
         public int solution(int[][] triangle) {
             int answer = 0;
-            int[][] temp = new int[triangle.length][triangle[triangle.length-1].length];
+            int[][] temp = new int[triangle.length][triangle[triangle.length - 1].length];
             temp[0][0] = triangle[0][0];
 
             for (int i = 1; i < triangle.length; i++) {
-                for (int j = 0; j <= i; j++) {
-                    if(j == 0)
-                        temp[i][j] = temp[i - 1][j] + triangle[i][j];
-                    else if(i == j)
-                        temp[i][j] = temp[i - 1][j - 1] + triangle[i][j];
-                    else
-                        temp[i][j] = Math.max(temp[i - 1][j - 1] + triangle[i][j], temp[i - 1][j] + triangle[i][j]);
-
+                temp[i][0] = temp[i - 1][0] + triangle[i][0];
+                temp[i][i] = temp[i - 1][i - 1] + triangle[i][i];
+                for (int j = 1; j <= i - 1; j++) {
+                    temp[i][j] = Math.max(temp[i - 1][j - 1], temp[i - 1][j]) + triangle[i][j];
                     answer = Math.max(answer, temp[i][j]);
                 }
             }
