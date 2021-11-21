a98200638b5af371e27e98914047347f1f5901a8
ryukiyoun <ryukiyoun@naver.com>
2021-11-21 22:20:40 +0900

    feat : 프로그래머스 등굣길 알고리즘 풀이
    Solution Class
     - 3중 for에서 물 웅덩이가 있는 지역의 표시를 미리 Integer Max_Value로 표시 하여 2중 for문으로 수정

--- programmers등굣길/src/question/Main.java
+++ programmers등굣길/src/question/Main.java
@@ -10,19 +10,15 @@ public class Main {
         public int solution(int m, int n, int[][] puddles) {
             int[][] map = new int[n + 1][m + 1];
 
+            for (int[] puddle : puddles) map[puddle[1]][puddle[0]] = Integer.MAX_VALUE;
+
             map[1][1] = 1;
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= m; j++) {
-                    boolean f = false;
-                    for(int k=0; k< puddles.length; k++) {
-                        if(puddles[k][1] == i && puddles[k][0] == j)
-                            f = true;
-                    }
-
-                    if (!f && !(i == 1 && j == 1))
-                        map[i][j] = (map[i - 1][j] + map[i][j - 1]) % 1000000007;
-                    else if(f)
+                    if(map[i][j] == Integer.MAX_VALUE)
                         map[i][j] = 0;
+                    else if (!(i == 1 && j == 1))
+                        map[i][j] = (map[i - 1][j] + map[i][j - 1]) % 1000000007;
                 }
             }
 
