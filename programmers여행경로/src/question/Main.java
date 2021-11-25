102c7471d0aba6ffc7981c863daab8cf1590ac19
ryukiyoun <ryukiyoun@naver.com>
2021-11-25 23:57:24 +0900

    feat : 프로그래머스 여행경로 알고리즘 풀이
    Solution Class
     - BFS를 이용하여 시작 공항에서 종료 공항까지 연결 될 수 있는 여행 경로를 찾고 해당 여행경로를 ,를 포함하도록 문자열로 만들어 List에 추가 후 List를 정렬하여 사전 순으로 가장 앞에 있는 Index 0번째의 List문자열을 ,로 Split 하여 문제 해결

---
+++ programmers여행경로/src/question/Main.java
@@ -0,0 +1,47 @@
+package question;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(new String[][] {{"ICN", "SFO"}, {"SFO", "ATL"}, {"SFO", "CTO"}, {"CTO", "HMM"}, {"ATL","ABC"}}));
+        System.out.println(solution.solution(new String[][] {{"ICN", "B"}, {"B", "ICN"}, {"ICN", "A"}, {"A", "D"}, {"D", "A"}}));
+        System.out.println(solution.solution(new String[][] {{"ICN", "AAA"}, {"ICN", "AAA"}, {"ICN", "AAA"}, {"AAA", "ICN"}, {"AAA", "ICN"}}));
+        System.out.println(solution.solution(new String[][] {{"ICN", "COO"}, {"ICN", "BOO"}, {"COO", "ICN"}, {"BOO", "DOO"}}));
+        System.out.println(solution.solution(new String[][] {{"ICN", "SFO"}, {"SFO", "ICN"}, {"ICN", "SFO"}, {"SFO", "QRE"}}));
+        System.out.println(solution.solution(new String[][] {{"ICN", "BOO"}, {"ICN", "COO"}, {"COO", "DOO"}, {"DOO", "COO"}, {"BOO", "DOO"}, {"DOO", "BOO"}, {"BOO", "ICN"}, {"COO", "BOO"}}));
+    }
+
+    public static class Solution {
+        private List<String> list;
+        public String[] solution(String[][] tickets) {
+            list = new ArrayList<>();
+
+            for(int i=0; i< tickets.length; i++) {
+                boolean[] check = new boolean[tickets.length];
+                if(tickets[i][0].equals("ICN")) {
+                    check[i] = true;
+                    BFS(tickets[i][1], check, tickets, tickets[i][0]);
+                }
+            }
+
+            Collections.sort(list);
+
+            return list.get(0).split(",");
+        }
+
+        public void BFS(String dest, boolean[] check, String[][] tickets, String result){
+            for(int i=0; i< tickets.length; i++){
+                if(!check[i] && tickets[i][0].equals(dest)){
+                    check[i] = true;
+                    BFS(tickets[i][1], check, tickets, result + "," + dest);
+                    check[i] = false;
+                }
+            }
+
+            if(result.split(",").length == tickets.length)
+                list.add(result + "," + dest);
+        }
+    }
+}
