9355e1107ef93bf835cb0577d110c6561f99b67d
ryukiyoun <ryukiyoun@naver.com>
2021-12-07 18:50:46 +0900

    feat : 프로그래머스 길 찾기 게임 알고리즘 풀이
    Solution Class
     - 입력 좌표 값을 Y값을 내림차순으로 정렬하고 Y값이 같은 경우는 X값을 내리차순으로 정렬한 뒤 Queue에 해당 좌표 및 번호를 Element로 넣고 정렬 Queue의 가장 앞에 있는 데이터가 Root가 되므로 Root 노드를 생성하고 X값을 비교하여 왼쪽과 오른쪽 노드의 위치를 선정하여 노드를 추가하도록 하여 트리를 구성 구성된 트리를 전위 순회와 후위 순회를 통해 배열을 만들고 해당 값들을 2차원 배열에 넣어 반환 하여 문제 풀이

---
+++ programmers길찾기게임/src/question/Main.java
@@ -0,0 +1,87 @@
+package question;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Solution solution = new Solution();
+        System.out.println(solution.solution(new int[][] {{10,6},{13,5},{8,4}}));
+    }
+
+    public static class Solution {
+        public int[][] solution(int[][] nodeinfo) {
+            PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) -> {
+                if(o1[1] == o2[1])
+                    return Integer.compare(o2[0], o1[0]);
+                else
+                    return Integer.compare(o2[1], o1[1]);
+            });
+
+            for(int i=0; i<nodeinfo.length; i++){
+                queue.offer(new int[]{nodeinfo[i][0], nodeinfo[i][1], i + 1});
+            }
+
+            int[] rootInfo = queue.poll();
+            Node root = new Node(rootInfo[0], rootInfo[2]);
+
+            while(!queue.isEmpty()){
+                int[] nodeInfo = queue.poll();
+                addNode(root, new Node(nodeInfo[0], nodeInfo[2]));
+            }
+
+            List<Integer> preAnswer = new LinkedList<>();
+            preOrder(root, preAnswer);
+
+            List<Integer> postAnswer = new LinkedList<>();
+            postOrder(root, postAnswer);
+
+            return new int[][]{preAnswer.stream().mapToInt(Integer::intValue).toArray(), postAnswer.stream().mapToInt(Integer::intValue).toArray()};
+        }
+
+        public void addNode(Node parent, Node child){
+            if(parent.x < child.x){
+                if(parent.right == null) parent.right = child;
+                else addNode(root.right, child);
+            }
+            else{
+                if(parent.left == null) parent.left = child;
+                else addNode(parent.left, child);
+            }
+        }
+
+        public void preOrder(Node node, List<Integer> answer){
+            if(node != null){
+                answer.add(node.number);
+                preOrder(node.left, answer);
+                preOrder(node.right, answer);
+            }
+        }
+
+        public void postOrder(Node node, List<Integer> answer){
+            if (node != null) {
+                postOrder(node.left, answer);
+                postOrder(node.right, answer);
+                answer.add(node.number);
+            }
+        }
+
+        public class Node{
+            private final int x;
+            private final int number;
+            private Node left;
+            private Node right;
+
+            public Node(int x, int number){
+                this.x = x;
+                this.number = number;
+                this.left = null;
+                this.right = null;
+            }
+        }
+    }
+}
