623dce427339d61879e6950637be818f7e228315
ryukiyoun <ryukiyoun@naver.com>
2021-12-09 18:19:06 +0900

    feat : 프로그래머스 N-Queen 알고리즘 풀이
    Solution Class
     - 퀸은 상하 좌우 대각선 모두로 움직일 수 있다.
     그래서 각 행에 퀸이 올 수 있는지 없는지 확인 후 올 수 있다면 해당 행에는 퀸을 두고 다음 행을 확인하는 방법으로 문제 풀이

--- programmersNQueen/src/question/Main.java
+++ programmersNQueen/src/question/Main.java
@@ -2,15 +2,12 @@ package question;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Solution solution = new Solution();
         System.out.println(solution.solution(4));
-        //System.out.println(solution.solution(5));
     }
 
     public static class Solution {
