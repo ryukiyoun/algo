f5eb72860623ce2f8dff0e62653e463c8d8b9d59
ryukiyoun <ryukiyoun@naver.com>
2022-05-24 20:19:22 +0900

    feat : 백준 알고리즘 풀이
    Solution Class
     - 백준 1932번

---
+++ back1932/src/question/Main.java
@@ -0,0 +1,38 @@
+package question;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer stringTokenizer;
+
+        int height = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[height][height];
+
+        dp[0][0] = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<height; i++){
+            stringTokenizer = new StringTokenizer(br.readLine());
+
+            dp[i][0] = dp[i-1][0] + Integer.parseInt(stringTokenizer.nextToken());
+
+            for(int j=1; j<=i; j++){
+                int num = Integer.parseInt(stringTokenizer.nextToken());
+                dp[i][j] = num + Math.max(dp[i-1][j-1], dp[i-1][j]);
+            }
+        }
+
+        Arrays.sort(dp[height-1]);
+
+        bw.write(dp[height-1][height-1] + "\n");
+        bw.flush();
+    }
+}
