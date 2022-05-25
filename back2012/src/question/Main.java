30b63c88783abc8714a0c541b5b9c2a92a1c0b90
ryukiyoun <ryukiyoun@naver.com>
2022-05-25 21:58:35 +0900

    feat : 백준 알고리즘 풀이
    Solution Class
     - 백준 2012번

---
+++ back2012/src/question/Main.java
@@ -0,0 +1,25 @@
+package question;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        long[] arr = new long[num + 1];
+
+        for (int i = 1; i <= num; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+
+        Arrays.sort(arr);
+
+        long result = 0;
+        for (int i = 1; i <= num; i++)
+            result += Math.abs(arr[i] - i);
+
+        System.out.println(result);
+    }
+}
