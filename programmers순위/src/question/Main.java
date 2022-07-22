56d3cd084340e7b59ce3884a46d473d766b4c2d0
ryukiyoun <ryukiyoun@naver.com>
2022-07-22 21:35:28 +0900

    feat : 프로그래머스 풀이
    Solution Class
     - 순위 풀이

---
+++ programmers순위/src/question/Main.java
@@ -0,0 +1,14 @@
+package question;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(new Solution().solution(5, new int[][]{{4, 3}, {4, 2}, {3, 2}, {1, 2}, {2, 5}}));
+    }
+
+    public static class Solution {
+        public int solution(int n, int[][] results) {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
