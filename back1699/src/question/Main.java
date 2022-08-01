968e8f58af02aee4ef71ebb50cdcf31bb36f15b3
ryukiyoun <ryukiyoun@naver.com>
2022-08-01 22:21:52 +0900

    feat : 백준 풀이
    Solution Class
     - 1699 속도 개선

--- back1699/src/question/Main.java
+++ back1699/src/question/Main.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
-//todo : 속도 개선이 필요함
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
