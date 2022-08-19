270ca734db736a6a4fe8e1dbb1b446340980c3cd
ryukiyoun <ryukiyoun@naver.com>
2022-08-19 17:53:16 +0900

    feat : 백준 알고리즘 풀이
    Solution Class
     - 백준 11055번

--- back11055/src/question/Main.java
+++ back11055/src/question/Main.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-
+//재풀이
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
